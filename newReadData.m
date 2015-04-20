6f6710c23aefe08a2ee837d46f61f5b4cb6f03fa
wislish <wislish@sina.com>
2015-04-20 16:03:46 +0800

    newReadData

--- newReadData.m
+++ newReadData.m
@@ -1,15 +1,12 @@
-fileFolder=fullfile('H:\大学学习\大四上\Projects\code\Geolife Trajectories 1.3\Geolife Trajectories 1.3\Data');
+fileFolder=fullfile('D:\Projects\code\Geolife Trajectories 1.3\Geolife Trajectories 1.3\Data');
 dirOutput=dir(fullfile(fileFolder,'*'));
 fileNames={dirOutput.name};
 fileNames(:,[1,2])=[];
 
-users = cell(182,1);
+users = cell(92,1);
 num=0;
- for userfile = 1:length(fileNames)
+for userfile = 1:length(fileNames)
     
-    if userfile>90
-        break;
-    end
     user = cell(3,1);
     lat=[];
     lon =[];
@@ -17,31 +14,35 @@ num=0;
     time=[];
     datetime=[];
     capacity=0;
-    file = dir(['H:\大学学习\大四上\Projects\code\Geolife Trajectories 1.3\Geolife Trajectories 1.3\Data\',fileNames{userfile},'\Trajectory\*.plt']);
-%     disp([fileNames{userfile},' ',file(length(file)).name ]);
-%     if ismember(1,strfind(file(length(file)).name,'2013'))
-%         num=num+1;
-%     end
-%     disp(num);
-    for n = 1:length(file)  
-        
+    file = dir(['D:\Projects\code\Geolife Trajectories 1.3\Geolife Trajectories 1.3\Data\',fileNames{userfile},'\Trajectory\*.plt']);
+    %     disp([fileNames{userfile},' ',file(length(file)).name ]);
+    %     if ismember(1,strfind(file(length(file)).name,'2013'))
+    %         num=num+1;
+    %     end
+    %     disp(num);
+    
+    for n = 1:length(file)
         capacity=capacity + file(n).bytes;
-        if capacity >2000000
-            break;
-        end
-        [blat blon bc3 balt bdateInDays bdate btime] = textread(['H:\大学学习\大四上\Projects\code\Geolife Trajectories 1.3\Geolife Trajectories 1.3\Data\',fileNames{userfile},'\Trajectory\',file(n).name],'%f %f %d %s %f %s %s','delimiter',',','headerlines',6);
+    end
+    
+    if capacity > 13000000 || capacity < 500000
+        continue;
+    end
+    num=num+1;
+    
+    for n = 1:length(file)
+        [blat blon bc3 balt bdateInDays bdate btime] = textread(['D:\Projects\code\Geolife Trajectories 1.3\Geolife Trajectories 1.3\Data\',fileNames{userfile},'\Trajectory\',file(n).name],'%f %f %d %s %f %s %s','delimiter',',','headerlines',6);
         lat = [lat;blat];
         lon = [lon;blon];
         date = [date;bdate];
         time = [time;btime];
-
     end
     len = length(lat);
     for k = 1:len
-        datetime{k} = [date{k},' ',time{k}];   
+        datetime{k} = [date{k},' ',time{k}];
     end
     user{1,1}=lat;
     user{2,1}=lon;
     user{3,1}=datetime;
-    users{userfile,1}=user;
- end
+    users{num,1}=user;
+end
